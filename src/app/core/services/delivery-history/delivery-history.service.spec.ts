dc96cb39b2c8f2e4f6e4a757a5ead9085640db7b
Kauã Semenov <kaua.semenov@gmail.com>
2023-10-22 19:43:07 -0300

    test: add unit tests for deliveryhistoryservice

--- src/app/core/services/delivery-history/delivery-history.service.spec.ts
+++ src/app/core/services/delivery-history/delivery-history.service.spec.ts
@@ -1,5 +1,4 @@
 import { TestBed } from '@angular/core/testing';
-
 import { DeliveryHistoryService } from './delivery-history.service';
 
 describe('DeliveryHistoryService', () => {
@@ -12,5 +11,36 @@ describe('DeliveryHistoryService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should add a trip to history', () => {
+    const trip = {
+      origin: 'A1',
+      pickup: 'B1',
+      destination: 'C1',
+      totalTime: 10,
+    };
+    service.addToHistory(trip);
+    expect(service.getHistory()).toContain(trip);
+  });
+
+  it('should get history', () => {
+    const trip1 = {
+      origin: 'A1',
+      pickup: 'B1',
+      destination: 'C1',
+      totalTime: 10,
+    };
+    const trip2 = {
+      origin: 'X1',
+      pickup: 'Y1',
+      destination: 'Z1',
+      totalTime: 20,
+    };
+    service.addToHistory(trip1);
+    service.addToHistory(trip2);
+
+    const history = service.getHistory();
+    expect(history).toEqual([trip2, trip1]);
   });
 });
