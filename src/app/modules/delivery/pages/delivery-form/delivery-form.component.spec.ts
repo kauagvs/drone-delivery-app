94b2d327fd8d0d3e1c5918755f577e2c33045776
Kauã Semenov <kaua.semenov@gmail.com>
2023-10-22 19:56:56 -0300

    test: calculate fastest path in deliveryformcomponent

--- src/app/modules/delivery/pages/delivery-form/delivery-form.component.spec.ts
+++ src/app/modules/delivery/pages/delivery-form/delivery-form.component.spec.ts
@@ -16,9 +16,25 @@ describe('DeliveryFormComponent', () => {
 
     fixture = TestBed.createComponent(DeliveryFormComponent);
     component = fixture.componentInstance;
+
+    component.origin = 'A1';
+    component.pickup = 'B2';
+    component.destination = 'C3';
+    component.timeData = {
+      A1: { B2: 10, C3: 20 },
+      B2: { A1: 10, C3: 5 },
+      C3: { A1: 20, B2: 5 },
+    };
   }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should calculate the fastest path and update fastestPath and totalTime', () => {
+    component.calculateFastestPath();
+
+    expect(component.fastestPath).toEqual(['A1', 'B2', 'C3']);
+    expect(component.totalTime).toEqual(15);
   });
 });
